b6f4113f7f91d96bd64eb9e4afc7ec1ec5412511
albertshaji <alby@disroot.org>
2020-03-27 23:11:08 +0530

    skip first three links(navbar)

--- newtab.js
+++ newtab.js
@@ -1,5 +1,5 @@
 var x = document.links;
-for (var i = 0; i < x.length; i++) {
+for (var i = 3; i < x.length; i++) {
     if (x[i].hostname != window.location.hostname) {
         x[i].target = "_blank";
         x[i].rel = "noopener noreferrer";
